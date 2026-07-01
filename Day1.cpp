d4a086d451e350d9a70e7d57e25e732482de9e71
Bowen Wan <256463718+Bowen-Wan@users.noreply.github.com>
2026-07-01 14:20:57 -0700

    Add files via upload

---
+++ Day1.cpp
@@ -0,0 +1,27 @@
+
+//Temperature Converter
+//1. Welcome user
+//2. Ask for Celsuis
+//3. Convert to Fahrenheit
+//4. Display result
+
+
+
+#include <iostream>
+
+
+int main(){
+
+std::cout<<"Welcome to the Temperature Converter!\n";
+
+int Celsuis;
+std::cout<<"Please enter the temperature in Celsuis: ";
+std::cin >> Celsuis;
+
+int Farenheit = (Celsuis * 9/5) + 32;
+std::cout<<"The temperature in Farenheit is: "<<Farenheit;
+return 0;
+
+}
+
+
